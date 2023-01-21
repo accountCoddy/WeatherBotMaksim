4cb428b1c080c4e51fedb46007078d7bb4f0ee53
Dmitry <dmitry.sh23@gmail.com>
2023-01-21 23:38:07 +0300

    1

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const TelegramBot = require('node-telegram-bot-api')
 const weather = require('./weather.js')
 const formatter_weather_data = require('./formatter-weather-data.js')
 
-const token = '5343486699:AAGeSKoSmjoVKhyJZO0fj-22vq2QEspF5H8'
+const token = '5343486699:AAGDIZDVvV_qGmR-sSkVzGPcH9pOmLj1xWY'
 
 const bot = new TelegramBot(token, {polling: true})
 
 const start = ()=>{
+    console.log("Bot starting")
     bot.on('message', async msg =>{
         let chatId = msg.chat.id
         let text = msg.text
